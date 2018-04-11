8550ebcfc04a8aaf2d9f2880568577dafd52461a
jmalish <jordanmalish@gmail.com>
2018-04-10 22:08:22 -0400

    Delete unneccessary files, move api call to background.js WIP

--- background.js
+++ background.js
@@ -1,1 +1,81 @@
-console.log("test");
+console.log("Loaded!");
+
+var apiURL = "strava.jordanmalish.com";
+var url = "https://www.strava.com/oauth/authorize?client_id=24632&response_type=code&redirect_uri=http://" +
+    apiURL + "&approval_prompt=auto&scope=public";
+
+
+
+var code;
+
+getStravaUserCode(url);
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.getCode !== "") {
+            sendResponse({code: "I'm gettin' a code!"});
+            // getStravaUserCode(url);
+        }
+    });
+
+function xgetStravaUserCode(url) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
+    xhr.setRequestHeader('content-type', 'text/plain');
+    console.log(xhr.redirectUrl);
+    xhr.onreadystatechange = function() {
+        console.log(xhr.readyState);
+        console.log(xhr.redirectUrl);
+    };
+
+    xhr.onprogress = function () {
+        console.log(xhr.redirectUrl);
+    };
+
+    xhr.send();
+}
+
+function getStravaUserCode(url) {
+    if (code) {
+        return code;
+    }
+
+
+    var options = {
+        'interactive': true,
+        'url': url
+    };
+
+    // chrome.identity.launchWebAuthFlow(options, function (responseUrl) {
+    //     console.log('launchWebAuthFlow completed', chrome.runtime.lastError,
+    //         responseUrl);
+    //
+    //     if (chrome.runtime.lastError) {
+    //         callback(new Error(chrome.runtime.lastError));
+    //     }
+    // });
+
+    chrome.identity.launchWebAuthFlow(options, function(redirectUri) {
+        console.log(options.url);
+        console.log('launchWebAuthFlow completed', chrome.runtime.lastError,
+            redirectUri);
+
+        if (chrome.runtime.lastError) {
+            callback(new Error(chrome.runtime.lastError));
+            return;
+        }
+
+        // Upon success the response is appended to redirectUri, e.g.
+        // https://{app_id}.chromiumapp.org/provider_cb#access_token={value}
+        //     &refresh_token={value}
+        // or:
+        // https://{app_id}.chromiumapp.org/provider_cb#code={value}
+        var matches = redirectUri.match(redirectRe);
+        if (matches && matches.length > 1)
+            handleProviderResponse(parseRedirectFragment(matches[1]));
+        else
+            callback(new Error('Invalid redirect URI'));
+    });
+}
--- content.js
+++ content.js
@@ -1,8 +1,7 @@
 var activityCards = document.getElementsByClassName('activity');
 
-var url = "https://www.strava.com/oauth/authorize?client_id=24632&response_type=code&redirect_uri=http://strava.jordanmalish.com&approval_prompt=auto&scope=public";
+thisTest();
 
-getStravaUserCode(url);
 
 for (var i = 0; i < activityCards.length; i++) {
     var activityID = activityCards[i].id.split("-")[1]; // get ID from element
@@ -10,28 +9,15 @@ for (var i = 0; i < activityCards.length; i++) {
     var mediaDiv = activityCards[i].getElementsByClassName("entry-body")[0].getElementsByClassName("media")[0]; // get the media div inside the entry-body div
 
     var weatherDiv = document.createElement('div'); // create our weather div
-    weatherDiv.innerHTML = '<p>This is my div4!</p>'; // input html of our div
+    weatherDiv.innerHTML = '<p>This is my div!</p>'; // input html of our div
     weatherDiv.className = "stravaWeather"; // give our div a name
 
     mediaDiv.parentNode.insertBefore(weatherDiv, mediaDiv.nextSibling); // insert our weather div after the media div
 }
 
-
-
-function getStravaUserCode(url) {
-    // var r = request.get(url, function (err, res, body) {
-    //     console.log(r.uri.href);
-    //     console.log(res.request.uri.href);
-    //     console.log(this.uri.href);
-    // });
-    var xhr = new XMLHttpRequest();
-
-    xhr.open('GET', url, true);
-    xhr.onload = function() {
-        if (xhr.readyState === 4) {
-            console.log(xhr.redirectUrl);
-        }
-    };
-    xhr.send();
+function thisTest() {
+    chrome.runtime.sendMessage({getCode: "code"}, function(response) {
+        console.log(response.code);
+    });
 }
 
