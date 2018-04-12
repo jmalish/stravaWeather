f96be350904f3c69ccd91bcf3a37325ce5b179cf
jmalish <jordanmalish@gmail.com>
2018-04-11 22:11:49 -0400

    Fix code retrieval

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
 console.log("Loaded!");
 
-var apiURL = "strava.jordanmalish.com";
-var url = "https://www.strava.com/oauth/authorize?client_id=24632&response_type=code&redirect_uri=http://" +
-    apiURL + "&approval_prompt=auto&scope=public";
+// var apiURL = "strava.jordanmalish.com";
+var url = "https://www.strava.com/oauth/authorize?client_id=24632&response_type=code&approval_prompt=auto&scope=public&redirect_uri=";
 
 
 
@@ -42,40 +41,16 @@ function getStravaUserCode(url) {
         return code;
     }
 
+    url = url + chrome.identity.getRedirectURL();
 
+    console.log(url);
     var options = {
         'interactive': true,
         'url': url
     };
 
-    // chrome.identity.launchWebAuthFlow(options, function (responseUrl) {
-    //     console.log('launchWebAuthFlow completed', chrome.runtime.lastError,
-    //         responseUrl);
-    //
-    //     if (chrome.runtime.lastError) {
-    //         callback(new Error(chrome.runtime.lastError));
-    //     }
-    // });
-
     chrome.identity.launchWebAuthFlow(options, function(redirectUri) {
-        console.log(options.url);
-        console.log('launchWebAuthFlow completed', chrome.runtime.lastError,
-            redirectUri);
-
-        if (chrome.runtime.lastError) {
-            callback(new Error(chrome.runtime.lastError));
-            return;
-        }
-
-        // Upon success the response is appended to redirectUri, e.g.
-        // https://{app_id}.chromiumapp.org/provider_cb#access_token={value}
-        //     &refresh_token={value}
-        // or:
-        // https://{app_id}.chromiumapp.org/provider_cb#code={value}
-        var matches = redirectUri.match(redirectRe);
-        if (matches && matches.length > 1)
-            handleProviderResponse(parseRedirectFragment(matches[1]));
-        else
-            callback(new Error('Invalid redirect URI'));
+        code = redirectUri.split("code=")[1];
+        console.log("User code = " + code);
     });
 }
